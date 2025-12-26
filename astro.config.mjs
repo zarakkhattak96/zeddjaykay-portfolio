17257316a9d6ae07bb9d67afddec08682b07911d
Zarak Khattak <khattakzarakj@outlook.com>
2025-12-26 20:01:12 +0500

    update: deprecatedf import error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/static";
+import vercel from "@astrojs/vercel";
 
 export default defineConfig({
 	output: "static",
 	adapter: vercel(),
+	image: {
+		service: {
+			entrypoint: "astro/assets/services/noop",
+		},
+	},
 });
